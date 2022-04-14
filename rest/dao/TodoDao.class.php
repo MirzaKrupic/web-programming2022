6f54ceb8c4dd2c03aa3b46539d3feb929396d43e
Mirza Krupic <krupicmirza99@gmail.com>
2022-04-14 09:21:34 +0200

    formatting code, adding basedao and services

--- rest/dao/TodoDao.class.php
+++ rest/dao/TodoDao.class.php
@@ -1,66 +1,17 @@
 <?php
+require_once __DIR__.'/BaseDao.class.php';
 
-class TodoDao{
-
-  private $conn;
-
+class TodoDao extends BaseDao{
   /**
   * constructor of dao class
   */
   public function __construct(){
-    $servername = "localhost";
-    $username = "todo";
-    $password = "todo";
-    $schema = "todo";
-    $this->conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
-    // set the PDO error mode to exception
-    $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    parent::__construct("todos");
   }
 
-  /**
-  * Method used to read all todo objects from database
-  */
-  public function get_all(){
-    $stmt = $this->conn->prepare("SELECT * FROM todos");
-    $stmt->execute();
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  public function get_todos_by_note_id($note_id){
+    return $this->query("SELECT * FROM todos WHERE note_id = :note_id", ['note_id' => $note_id]);
   }
-
-  public function get_by_id($id){
-    $stmt = $this->conn->prepare("SELECT * FROM todos WHERE id = :id");
-    $stmt->execute(['id' => $id]);
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    return reset($result);
-  }
-
-  /**
-  * Method used to add todo to the database
-  */
-  public function add($todo){
-    $stmt = $this->conn->prepare("INSERT INTO todos (description, created) VALUES (:description, :created)");
-    $stmt->execute($todo);
-    $todo['id'] = $this->conn->lastInsertId();
-    return $todo;
-  }
-
-  /**
-  * Delete todo record from the database
-  */
-  public function delete($id){
-    $stmt = $this->conn->prepare("DELETE FROM todos WHERE id=:id");
-    $stmt->bindParam(':id', $id); // SQL injection prevention
-    $stmt->execute();
-  }
-
-  /**
-  * Update todo record
-  */
-  public function update($todo){
-    $stmt = $this->conn->prepare("UPDATE todos SET description=:description, created=:created WHERE id=:id");
-    $stmt->execute($todo);
-    return $todo;
-  }
-
 }
 
 ?>
